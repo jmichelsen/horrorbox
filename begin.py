ea29ce60d21b50a2dcc7b6185f23e2e4237658d1
Jesse Michelsen <jmichelsen4@gmail.com>
2017-09-26 13:49:01 -0600

    Initial begin.py that plays music and nothing more

---
+++ begin.py
@@ -0,0 +1,12 @@
+from music import HorrorsEar
+
+
+if __name__ == '__main__':
+    horrors_ear = HorrorsEar()
+    horrors_ear.begin()
+    try:
+        while True:
+            continue
+    except KeyboardInterrupt:
+        pass
+
